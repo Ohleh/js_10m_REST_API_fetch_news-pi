79122fba0fd6710a79faf1ca1c51dbd7a32ee1a5
Ohleh <oleg.kolischuk@gmail.com>
2024-04-07 17:41:42 +0300

    add fetch news

--- js/newsApi.js
+++ js/newsApi.js
@@ -4,21 +4,62 @@
 // resource
 // parameters
 
-const ulRef = document.querySelector(".news__list");
-const KEY = "04817410139a4f28b3432ba663d88446";
-const BASE_URL = "https://newsapi.org/v2";
-const URL = `${BASE_URL}/top-headlines?country=us&pageSize=10&apiKey=${KEY}`;
-
+// == 1 tsk
 // function fetch
 // (function checkStatus ok ?)
 // function createItem
 // function createList
 // function renderContent
+// == 1 end
 
-fetch(URL)
-  .then((response) => checkStatus(response))
-  .then((data) => renderContent(data.articles))
-  .catch((e) => console.log("error:", e));
+// == 2 tsk
+// (зчитати дані)
+// поставити обробник події на сабміт форми, фетч по кнопці (сабміті)
+// зчитати дані з селекта і інпута
+// підставити ці дані в ссилку для оновлення по сабміту
+// тест рендера
+// == 2 end
+
+// == 3 tsk
+// (пагінація)
+// вивести текст кількості сторінок і новин (окрема функція)
+// додати кнопку load more
+
+const formRef = document.querySelector(".formNews");
+const categoryRef = document.querySelector(".category");
+const pageSizeRef = document.querySelector(".pageSize");
+const totalResult = document.querySelector(".totalResults");
+const totalPage = document.querySelector(".totalPages");
+const loadMore = document.querySelector(".loadMore");
+const ulRef = document.querySelector(".newsList");
+const KEY = "04817410139a4f28b3432ba663d88446";
+const BASE_URL = "https://newsapi.org/v2";
+
+const pagesInfo = (data, pageSize) => {
+  totalResult.textContent = `Total news number: ${data?.totalResults}.`;
+  totalPage.textContent = `Total pages: ${Math.ceil(
+    data?.totalResults / pageSize
+  )}.`;
+};
+
+const onSubmit = (e) => {
+  e.preventDefault();
+  const category = categoryRef.value;
+  const pageSize = pageSizeRef.value;
+
+  const URL = `${BASE_URL}/top-headlines?country=us&category=${category}&pageSize=${pageSize}&apiKey=${KEY}`;
+
+  fetch(URL)
+    .then((response) => checkStatus(response))
+    .then((data) => {
+      ulRef.textContent = "";
+      pagesInfo(data, pageSize);
+      renderContent(data.articles);
+    })
+    .catch((e) => console.log("error:", e));
+};
+
+formRef.addEventListener("submit", onSubmit);
 
 const checkStatus = (res) => {
   if (!res.ok) {
@@ -29,9 +70,10 @@ const checkStatus = (res) => {
 
 const createItem = (article) => `<li>
 <img src=${article.urlToImage} alt=${article.description} style=height:300>
-<p> ${article.title ?? ""}</p>
+<h2> ${article.title.slice(0, 40) ?? ""}...</h2>
 <p> ${article.description ?? ""}</p>
 ${article.author ? `<p>${article.author}</p>` : ""}
+<a href=${article.url}>read more</a>
 </li>`;
 
 const renderList = (dataArr) =>
